d0a1476f4b304b98f07d59fdd1b2257fba2f3806
Olena457 <olenademianenko42@gmail.com>
2026-07-19 17:59:14 +0300

    feat: deploy backend and frontend added connection url

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
         "http://localhost:3000", 
         "http://127.0.0.1:3000", 
         "http://192.168.1.3:3000",
-        "https://tviy-frontend-na-vercel.app"
+        "https://python-taskflow.vercel.app"
         
 
     ], 
